65f99759e579caac3dfef6e0dbb3dbb04916435b
anhdodz2012-maker <anhdodz2012@gmail.com>
2025-11-12 16:27:10 +0700

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trang Web Đầu Tiên</title>
+</head>
+<body>
+  <h1>Xin chào! Đây là website đầu tiên của mình 🎉</h1>
+  <p>Tạo miễn phí bằng GitHub Pages.</p>
+</body>
+</html>
